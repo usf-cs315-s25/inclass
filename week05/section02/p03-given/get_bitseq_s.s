5bf6f2d4dcab7bdffc6ef6506d0987d81ce3bfc0
Phil Peterson <phpeterson@usfca.edu>
2025-02-19 15:32:15 -0800

    add s02 2/19

--- week05/section02/p03-given/get_bitseq_s.s
+++ week05/section02/p03-given/get_bitseq_s.s
@@ -1,4 +1,16 @@
 .global get_bitseq_s
 
+# a0 = num, a1 = start, a2 = end
 get_bitseq_s:
+
+    sub t0, a2, a1      # t0 is end - start
+    addi t0, t0, 1      # t0 is len
+
+    srl t1, a0, a1      # t1 is val (num >> start)
+    
+    li t2, 1            # t2 = 1 to shift
+    sll t2, t2, t0      # t2 = 1 << len
+    addi t2, t2, -1     # t2 is the mask
+
+    and a0, t1, t2      # apply mask to ret val
     ret
