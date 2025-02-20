64995ce15cb200f7d37cfb05cdce7cf26bc407cd
Phil Peterson <phpeterson@usfca.edu>
2025-02-20 09:42:36 -0800

    add 2/20 s01

--- week05/section01/p03-given/get_bitseq_c.c
+++ week05/section01/p03-given/get_bitseq_c.c
@@ -3,13 +3,13 @@
 uint32_t get_bitseq_c(uint32_t num, int start, int end) {
     uint32_t val;
     int len;
-    uint32_t mask;
+    uint64_t mask;
 
     len = (end - start) + 1;
     val = num >> start;
 
-    if (len == 32) {
-        mask = 0xFFFFFFFF;
+    if (len == 64) {
+        mask = 0xFFFFFFFFFFFFFFFF;
     } else {
         mask = (0b1 << len) - 1;
     }
--- week05/section01/p03-given/get_bitseq_s.s
+++ week05/section01/p03-given/get_bitseq_s.s
@@ -7,10 +7,17 @@ get_bitseq_s:
 
     srl t1, a0, a1          # t1 is num >> start
 
+    li t4, 64
+    bne t4, t0, calc_mask
+    li t2, 0xFFFFFFFFFFFFFFFF
+    j apply_mask
+
+calc_mask:
                             # calculate mask
     li t2, 1                # t2 is 1
     sll t2, t2, t0          # t2 = 1 << len
     addi t2, t2, -1         # t2 is mask
 
+apply_mask:
     and a0, t1, t2          # apply mask, isolating bits
     ret
